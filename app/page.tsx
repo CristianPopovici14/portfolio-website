e09affb4054fefde921fec7881619f20a7ed8d3b
cristian.popovici54 <cristian.popovici54@gmail.com>
2025-07-22 10:18:13 +0000

    Added github links

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
                   <div className="bg-zinc-800 w-full h-fit rounded-xl p-4 mb-4">
                       <Image src="/QuarkBrowser.png" width={500} height={500} className="w-full rounded-md" alt="olaNotes image"/>
                       <h3 className="text-white font-bold text-xl pb-3">Quark Browser</h3>
-                      <a href="" className="text-white font-bold flex items-center hover:text-zinc-200 w-fit">GitHub <SquareArrowOutUpRightIcon className="ml-1 h-3 w-3" /></a>
+                      <a href="https://github.com/CristianPopovici14/quark-browser" className="text-white font-bold flex items-center hover:text-zinc-200 w-fit">GitHub <SquareArrowOutUpRightIcon className="ml-1 h-3 w-3" /></a>
                       <p className="text-white mt-1">
                           A browser made with <strong>HTML</strong>, <strong>Tailwind CSS</strong>, <strong>JavaScript</strong> and <strong>Electron</strong>.
                       </p>
@@ -45,7 +45,7 @@ export default function Home() {
                   <div className="bg-zinc-800 w-full h-fit rounded-xl p-4 mb-4">
                       <Image src="/mywebsite.png" width={500} height={500} className="w-full rounded-md" alt="olaNotes image"/>
                       <h3 className="text-white font-bold text-xl pb-3">This Website</h3>
-                      <a href="" className="text-white font-bold flex items-center hover:text-zinc-200 w-fit">GitHub <SquareArrowOutUpRightIcon className="ml-1 h-3 w-3" /></a>
+                      <a href="https://github.com/CristianPopovici14/portfolio-website" className="text-white font-bold flex items-center hover:text-zinc-200 w-fit">GitHub <SquareArrowOutUpRightIcon className="ml-1 h-3 w-3" /></a>
                       <p className="text-white mt-1">
                           A portfolio website made with <strong>NextJS</strong>, <strong>React</strong>, <strong>Typescript</strong> and <strong>Shadcn/ui</strong>.
                       </p>
